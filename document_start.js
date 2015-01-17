12aaf2213207cc6411016c0bdb24540b0c8b95d1
Yoshiaki Kawazu <kawazzz@gmail.com>
2015-01-17 22:41:22 +0900

    リファクタリング&楽天注文履歴ページのセレクタ修正

--- document_start.js
+++ document_start.js
@@ -1,19 +1,36 @@
 [ {url:/^https?:\/\/www.google.(.*)\/search/, selector:"input[name=q]"}
-, {url:/^http:\/\/search.books.rakuten.co.jp\//, selector:"#searchWords"}
-, {url:/^https:\/\/order.my.rakuten.co.jp\//, selector:"input[name=search_item]"}
+, {url:/^https?:\/\/search.books.rakuten.co.jp\//, selector:"#searchWords"}
+, {url:/^https?:\/\/order.my.rakuten.co.jp\//, selector:"#inputSearch"}
+, {url:/^http:\/\/[\w\.]+\.alc.co.jp\//, selector:"#q"}
 ].forEach(function(o){
-    if(o.url.test(location.href)) {
-      addFocusTarget(o.selector);
-    }
+  if(o.url.test(location.href)) {
+    addFocusTarget(o.selector);
+  }
 });
 function addFocusTarget(selector) {
-  document.addEventListener("webkitvisibilitychange", function(){
-      if("visible" == document.webkitVisibilityState) {
-        var elm = document.querySelector(selector);
-        if(elm) {
-          elm.focus();
-          elm.select();
-        }
-      }
+  //ページ読み込み
+  document.addEventListener("DOMContentLoaded", function(){
+    var elm = document.querySelector(selector);
+    if(elm) {
+      //初回自動フォーカス
+      elm.focus();
+      elm.select();
+      //貼付け時自動検索追加
+      elm.addEventListener('paste', function(e){
+        setTimeout(function(){
+          elm.form && elm.form.submit();
+        }, 0);
+      });
+    }
   }, false);
+  //タブ移動時自動フォーカス
+  document.addEventListener("webkitvisibilitychange", function(){
+    if("visible" == document.webkitVisibilityState) {
+      var elm = document.querySelector(selector);
+      if(elm) {
+        elm.focus();
+        elm.select();
+      }
+    }
+  });
 }
